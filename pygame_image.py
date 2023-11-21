6237e259a2cbbbf80c55a1de3634f4db83e389c7
C0B22123 <C0B22123d2@edu.teu.ac.jp>
2023-11-21 17:07:11 +0900

    えんしゅうかだい１

--- pygame_image.py
+++ pygame_image.py
@@ -7,10 +7,13 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
-    bg_img2 = pg.image.load("ex01/fig/3.png")
-    bg_img2 =pg.transform.flip(bg_img2, True, False)
-    bg_img3= pg.transform.rotozoom(bg_img2, 10, 1.0)
-    lst=[bg_img2,bg_img3 ]
+    bg_img2=pg.transform.flip(bg_img, True, False)
+    kk_img2 = pg.image.load("ex01/fig/3.png")
+    kk_img2 =pg.transform.flip(kk_img2, True, False)
+    kk_img3=[kk_img2]
+    suuti=[2,4,6,8,10,12,10,8,6,4,2]
+    for i in suuti:
+        kk_img3.append(pg.transform.rotozoom(kk_img2, i, 1.0))
     tmr = 0
 
     while True:
@@ -19,12 +22,12 @@ def main():
         
         x=tmr%1600
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(lst[tmr%2],[300,200])
+        screen.blit(bg_img2, [1600-x, 0])
+        screen.blit(kk_img3[int(tmr%(len(suuti)*8)/8)],[300,200])
         
         pg.display.update()
         tmr += 1        
-        clock.tick(1000)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
