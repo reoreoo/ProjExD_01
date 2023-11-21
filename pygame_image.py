3089f7ef9541d3ba1a1883a08ec33f7aae1ffa77
C0B22123 <C0B22123d2@edu.teu.ac.jp>
2023-11-21 15:56:41 +0900

    れんしゅう6

--- pygame_image.py
+++ pygame_image.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
-    screen = pg.display.set_mode((1600, 900))
+    screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
     bg_img2 = pg.image.load("ex01/fig/3.png")
@@ -12,20 +12,19 @@ def main():
     bg_img3= pg.transform.rotozoom(bg_img2, 10, 1.0)
     lst=[bg_img2,bg_img3 ]
     tmr = 0
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        
-        screen.blit(bg_img, [0, 0])
+        x=tmr%1600
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img, [1600-x, 0])
         screen.blit(lst[tmr%2],[300,200])
-            
-
-        
         
         pg.display.update()
         tmr += 1        
-        clock.tick(10)
+        clock.tick(1000)
 
 
 if __name__ == "__main__":
